cd8a9e9427a56193e1b5b7c85d61ed89a14187f1
Kosuke Suzuki <kosuke.suzuki.127@gmail.com>
2019-09-23 04:29:06 +0900

    initial commit

---
+++ lib/method_call_logger.rb
@@ -0,0 +1,6 @@
+require "method_call_logger/version"
+
+module MethodCallLogger
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/method_call_logger/version.rb
@@ -0,0 +1,3 @@
+module MethodCallLogger
+  VERSION = "0.1.0"
+end
